37dd1b4ac17c4c69a76376677c138be4a2a972e8
wchoujaa <wchoujaa@ulb.ac.be>
2020-02-17 16:38:34 +0100

    .

--- game-dev-map/js/index.js
+++ game-dev-map/js/index.js
@@ -1,16 +1,208 @@
 const data_link = "https://raw.githubusercontent.com/wchoujaa/game-dev-map/master/readme.md";
-                    
-var data;
 
-var tree;
+var data;
+var cluster;
+var root;
+var svg;
 var treeLayout;
+
+var i = 0;
+var duration = 750;
+
 d3.select(window).on("load", async function () {
     await d3.text(data_link).then(function (text) {
         data = hierarchy_from(text);
     });
     //console.log(tree, data);
-    normalDendogram(data)
+    init()
 });
+
+function init() {
+    var width = window.screen.width
+    var height = window.screen.height
+    svg = d3.select("#dendo")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "scale(0.7) translate( " + width / 3 + "," + height / 7 + ")");
+
+    root = d3.hierarchy(data, function (d) { return d.children; });
+
+    cluster = d3.cluster()
+        .size([height, width * 0.7]);
+
+
+    root.children.forEach(collapse);
+    root.x0 = height / 2;
+    root.y0 = 0;
+    update(root);
+
+}
+
+function update(source) {
+
+    // Assigns the x and y position for the nodes
+    var treeData = cluster(root);
+
+    // Compute the new tree layout.
+    var nodes = treeData.descendants(),
+        links = treeData.descendants().slice(1);
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function (d) { d.y = d.depth * 180 });
+
+    // ****************** Nodes section ***************************
+
+    // Update the nodes...
+    var node = svg.selectAll('g.node')
+        .data(nodes, function (d) { return d.id || (d.id = ++i); });
+
+    // Enter any new modes at the parent's previous position.
+    var nodeEnter = node.enter().append('g')
+        .attr('class', 'node')
+        .attr("transform", function (d) {
+            return "translate(" + source.y0 + "," + source.x0 + ")";
+        })
+        .on('click', click);
+
+    // Add Circle for the nodes
+    nodeEnter.append('circle')
+        .attr('class', 'node')
+        .attr('r', 1e-6)
+        .style("fill", function (d) {
+            return d._children ? "lightsteelblue" : "#fff";
+        });
+
+    // Add labels for the nodes
+    nodeEnter.append('text')
+        .attr("dy", ".35em")
+        .attr("x", function (d) {
+            return (d.data.name == "Game Developpement") ? -17 : 17;
+        })
+        .attr("text-anchor", (d) => {
+            return (d.data.name == "Game Developpement") ? "end" : "start";
+        })
+        .text(function (d) { return d.data.name; });
+
+    // UPDATE
+    var nodeUpdate = nodeEnter.merge(node);
+
+    // Transition to the proper position for the node
+    nodeUpdate.transition()
+        .duration(duration)
+        .attr("transform", function (d) {
+            return "translate(" + d.y + "," + d.x + ")";
+        });
+
+    // Update the node attributes and style
+    nodeUpdate.select('circle.node')
+        .attr('r', 10)
+        .style("fill", function (d) {
+            return d._children ? "lightsteelblue" : "#fff";
+        })
+        .attr('cursor', 'pointer');
+
+ 
+
+    // Remove any exiting nodes
+    var nodeExit = node.exit().transition()
+        .duration(duration)
+        .attr("transform", function (d) {
+            return "translate(" + source.y + "," + source.x + ")";
+        })
+        .remove();
+
+    // On exit reduce the node circles size to 0
+    nodeExit.select('circle')
+        .attr('r', 1e-6);
+
+    // On exit reduce the opacity of text labels
+    nodeExit.select('text')
+        .style('fill-opacity', 1e-6)
+
+
+    // ****************** links section ***************************
+
+    // Update the links...
+    var link = svg.selectAll('path.link')
+        .data(links, function (d) { return d.id; });
+
+    // Enter any new links at the parent's previous position.
+    var linkEnter = link.enter().insert('path', "g")
+        .attr("class", "link")
+        .attr('d', function (d) {
+            var o = { x: source.x0, y: source.y0 }
+            return diagonal(o, o)
+        });
+
+    // UPDATE
+    var linkUpdate = linkEnter.merge(link);
+
+    // Transition back to the parent element position
+    linkUpdate.transition()
+        .duration(duration)
+        .attr('d', function (d) { return diagonal(d, d.parent) });
+
+    // Remove any exiting links
+    var linkExit = link.exit().transition()
+        .duration(duration)
+        .attr('d', function (d) {
+            var o = { x: source.x, y: source.y }
+            return diagonal(o, o)
+        })
+        .remove();
+
+    // Store the old positions for transition.
+    nodes.forEach(function (d) {
+        d.x0 = d.x;
+        d.y0 = d.y;
+    });
+
+    // Creates a curved (diagonal) path from parent to the child nodes
+    function diagonal(s, d) {
+
+        path = `M ${s.y} ${s.x}
+            C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`
+
+        return path
+    }
+
+    // Toggle children on click.
+    function click(d) {
+        if (d.children) {
+            d._children = d.children;
+            d.children = null;
+        } else {
+            d.children = d._children;
+            d._children = null;
+        }
+        update(d);
+    }
+}
+
+
+
+
+function collapse(d) {
+    if (d.children) {
+        d._children = d.children
+        d._children.forEach(collapse)
+        d.children = null
+    }
+}
+
+function click(d) {
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+    } else {
+        d.children = d._children;
+        d._children = null;
+    }
+    update(d);
+}
 
 function normalDendogram(data) {
     var width = window.screen.width
@@ -77,16 +269,15 @@ function normalDendogram(data) {
         .attr("fill", "Black")
         .style("font", "normal 17px Arial")
         .attr("dy", ".35em")
-        .attr("text-anchor", "start")
-        .attr("transform", function (d) {
-            return "translate(17,0)"
-        })
+        .attr("text-anchor", (d) => (d.data.name == "Game Developpement") ? "end" : "start")
+        .attr("transform", (d) => (d.data.name == "Game Developpement") ? "translate(-17,0)" : "translate(17,0)")
         .text(function (d) {
             return d.data.name;
         });
 
 
 }
+
 
 function radialDendogram(data) {
     var width = window.screen.width
